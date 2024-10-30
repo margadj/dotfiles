c31235cc6e237186cc6b54503f1cf6124f6f520a
margadj <maagii1130@gmail.com>
2024-10-30 15:31:55 +0800

    update nvim config

---
+++ .config/nvim/lua/lazy-plugins.lua
@@ -0,0 +1,40 @@
+require('lazy').setup({
+  'tpope/vim-sleuth',
+
+  require 'plugins.tokyonight',
+  require 'plugins.gitsigns',
+  require 'plugins.which-key',
+  require 'plugins.telescope',
+  require 'plugins.lspconfig',
+  require 'plugins.autoformat',
+  require 'plugins.autocomplete',
+  require 'plugins.comments',
+  require 'plugins.mini',
+  require 'plugins.treesitter',
+  require 'plugins.debug',
+  require 'plugins.lint',
+  require 'plugins.indent_line',
+  require 'plugins.autopairs',
+  require 'plugins.neo-tree',
+  require 'plugins.gitsigns',
+}, {
+  ui = {
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
+
+-- vim: ts=2 sts=2 sw=2 et
