46c2fdf9e6a42483c96443359ed36160c845c4fd
margadj <maagii1130@gmail.com>
2024-10-30 13:12:13 +0800

    update nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -86,7 +86,6 @@ require('lazy').setup({
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
           ['<C-y>'] = cmp.mapping.confirm { select = true },
-
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
           --['<CR>'] = cmp.mapping.confirm { select = true },
